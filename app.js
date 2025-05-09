eb231899f62e1de745a8ab7015dc7a7b7fab107e
ferriccc <shrey.260904@gmail.com>
2025-05-09 15:59:51 +0530

    test

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ const PORT = 3000;
 
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || "https://bookrack.vercel.app/",
+    origin: process.env.FRONTEND_URL || "http://localhost:5173",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
